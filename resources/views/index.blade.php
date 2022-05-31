6eb55e22d04e874fe21ebba7fb842c16d94484f8
devcooljoe <onipedejoseph@outlook.com>
2022-05-31 13:02:31 +0100

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,327 @@
+@extends('layouts.app') @section('content')
+    <div class="top-intro-div row">
+        <div class="col-12 col-md-1 d-none d-sm-block"></div>
+        <div class="col-12 col-md-5 col-sm-12 col-xs-12 m-auto">
+            <div class="heading-desc" data-aos="fade-right" data-aos-duration="500" data-aos-delay="50">
+                <h1>Get Your WhatsApp Status Views Increased Daily in One Click</h1>
+                <p>Get more WhatsApp Status Views. You can grow your audience as you and other participants gets to save
+                    each others contact, Meet new friends, customers and business partners.</p>
+                <a href="/register" class="btn btn-success btn-lg">Get Started</a>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-sm-12 col-xs-12 m-auto text-center">
+            <img data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100" class="img-fluid m-auto"
+                src="{{ asset('images/holding-arrow.svg') }}" alt="">
+        </div>
+        <div class="col-12 col-md-1 d-none d-sm-block"></div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="container">
+        <h2 class="wv-section-3-title" data-aos="fade-right" data-aos-duration="1000">How it Works</h2>
+        <div class="row wv-section-3">
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="50">
+                <div class="m-auto">
+                    <img class="img-fluid" src="{{ asset('images/Frame.svg') }}" alt="">
+                    <h3 class="text-center">Submit Contact</h3>
+                    <p class="text-center">Submit your whatsapp number in order to be added among those that will get the
+                        VCF files</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="100" data-aos-easing="ease-in-out">
+                <div class="m-auto">
+                    <img class="img-fluid" src="{{ asset('images/contact-us-bro.svg') }}" alt="">
+                    <h3 class="text-center">Get VCF Daily</h3>
+                    <p class="text-center">Download your personal VCF files (All Users have their own VCF file)</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="200">
+                <div class="m-auto">
+                    <img class="img-fluid" src="{{ asset('images/cloud-sync-bro.svg') }}" alt="">
+                    <h3 class="text-center">Share Link</h3>
+                    <p class="text-center">Click the whatsapp share button in order to share our link</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="top-intro-div row background-primary img-fluid mt-5">
+        <div class="col-12 col-md-1 d-none d-sm-block"></div>
+        <div class="col-12 col-md-5 col-sm-12 col-xs-12 m-auto">
+            <div class="heading-desc" data-aos="fade-up" data-aos-duration="500" data-aos-delay="50">
+                <h3 class="text-white">Verify & Place Your Whatsapp TV Here #5,000 per month.</h3>
+                <p class="mb-2">Reasons to Verify and Place Tv here.</p>
+                <ul>
+                    <li><img class="img-fluid" src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}"
+                            alt=""> Gain the trust of your customers.</li>
+                    <li><img class="img-fluid" src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}"
+                            alt=""> Get our users to trust you.</li>
+                    <li><img class="img-fluid" src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}"
+                            alt=""> Boost your sale 100% assured</li>
+                    <li><img class="img-fluid" src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}"
+                            alt=""> Get your personal WhatsApp TV link on Wassapviews</li>
+                </ul>
+
+                <h3 class="text-white mt-5">Note:</h3>
+                <ul>
+                    <li><img class="img-fluid" src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}"
+                            alt=""> You must be a trusted Tv therefore your must upload a non-edited and raw video proof of
+                        your Status Views Countl</li>
+                    <li><img class="img-fluid" src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}"
+                            alt=""> You will be tested berore you will be approved</li>
+                    <li><img class="img-fluid" src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}"
+                            alt=""> There is no refund.</li>
+                </ul>
+                <a href="#" class="btn btn-success btn-lg">Register</a>
+            </div>
+        </div>
+        <div class="col-12 col-md-5 col-sm-12 col-xs-12 m-auto text-center">
+            <img data-aos="fade-up-left" data-aos-duration="1000" data-aos-delay="100" class="img-fluid text-center"
+                src="{{ asset('images/Enthusiastic-bro.svg') }}" alt="">
+        </div>
+        <div class="col-12 col-md-1 d-none d-sm-block"></div>
+    </div>
+    <div class="container mb-5">
+        <h2 class="wv-section-3-title fade-left" data-aos="fade-left" data-aos-duration="1000">Why Choose Us?</h2>
+        <div class="row wv-section-3">
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="50">
+                <div class="m-auto">
+                    <img class="img-fluid" src="{{ asset('images/Wristwatch-rafiki-1.svg') }}" alt="">
+                    <h3 class="text-center">Saves Time & Effort</h3>
+                    <p class="text-center">With Wassapviews, your status views will grow rapidly and you will never
+                        downoad duplicate contact.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="100" data-aos-easing="ease-in-out">
+                <div class="m-auto">
+                    <img class="img-fluid" src="{{ asset('images/Active-Supportbro1.svg') }}" alt="">
+                    <h3 class="text-center">24/7 Support</h3>
+                    <p class="text-center">Wassapviews work 24/7 in order to serve its users consistently.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="200">
+                <div class="m-auto">
+                    <img class="img-fluid" src="{{ asset('images/Customer-Survey-rafiki-1.svg') }}" alt="">
+                    <h3 class="text-center">Autmatic & Antiban</h3>
+                    <p class="text-center">It is automatic so you don’t have to message people before they save your
+                        number. It also has antiban features.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="top-intro-div row background-primary img-fluid mt-5">
+        <h3 class="text-white text-center mt-5">Benefits of having alot of status views</h3>
+        <div class="col-12 col-md-1 d-none d-sm-block"></div>
+        <div class="col-12 col-md-5 col-sm-12 col-xs-12 m-auto text-center">
+            <img data-aos="fade-up-right" data-aos-duration="1000" data-aos-delay="100" class="img-fluid text-center"
+                src="{{ asset('images/Partnership-bro-1.svg') }}" alt="">
+        </div>
+        <div class="col-12 col-md-5 col-sm-12 col-xs-12 m-auto">
+            <div class="heading-desc" data-aos="fade-up" data-aos-duration="500" data-aos-delay="50">
+                <ul>
+                    <li class="mb-3"><img class="img-fluid"
+                            src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}" alt=""> More popularity =
+                        More money.</li>
+                    <li class="mb-3"><img class="img-fluid"
+                            src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}" alt=""> More interesting
+                        statuses to view.</li>
+                    <li class="mb-3"><img class="img-fluid"
+                            src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}" alt=""> More sales for
+                        business owners.</li>
+                    <li class="mb-3"><img class="img-fluid"
+                            src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}" alt=""> More people get to
+                        know about you and your business.</li>
+                    <li class="mb-3"><img class="img-fluid"
+                            src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}" alt=""> If you're self
+                        employed, you can have better opportunities.</li>
+                    <li class="mb-3"><img class="img-fluid"
+                            src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}" alt=""> Helps you discover
+                        new sources of income.</li>
+                    <li class="mb-3"><img class="img-fluid"
+                            src="{{ asset('images/fluent_checkmark-circle-32-filled.svg') }}" alt=""> Helps you find more
+                        interesting people like you.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-12 col-md-1 d-none d-sm-block"></div>
+    </div>
+    <div class="slide-show-wv">
+        <div id="wvCarouselIndicator" class="carousel slide mt-2 mb-2" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#wvCarouselIndicator" data-slide-to="0" class="active"></li>
+                <li data-target="#wvCarouselIndicator" data-slide-to="1"></li>
+                <li data-target="#wvCarouselIndicator" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block img-thumbnail img-fluid"
+                        src="https://i.ibb.co/B4cVdQM/Whats-App-Image-2022-01-09-at-11-41-50-PM.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-thumbnail img-fluid"
+                        src="https://i.ibb.co/RSZhR4K/Whats-App-Image-2022-01-08-at-4-57-09-AM.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-thumbnail img-fluid"
+                        src="https://i.ibb.co/1GQ0q6Q/242380766-347583560437491-2882487378983355338-n.jpg"
+                        alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#wvCarouselIndicator" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" id="wvNext1" href="#wvCarouselIndicator" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <div class="container mb-5 testi">
+        <h2 class="wv-section-3-title" data-aos="fade-right" data-aos-duration="1000">Testimonies</h2>
+        <p class="text-center mb-5">See few Testimonies from Our Premium Users Below.</p>
+        <div class="row  testimony text-center justify-content-center">
+            <div class="col-12 col-md-3 col-sm-12 col-xs-12 m-3" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="50">
+                <img class="img-fluid img-zoom-out"
+                    src="https://i.ibb.co/LpgqtBb/Whats-App-Image-2021-09-22-at-9-49-49-PM.jpg" alt="">
+            </div>
+            <div class="col-12 col-md-3 col-sm-12 col-xs-12 m-3" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="100" data-aos-easing="ease-in-out">
+                <img class="img-fluid" src="https://i.ibb.co/qrkK2f9/Whats-App-Image-2021-09-21-at-2-59-19-AM.jpg"
+                    alt="">
+            </div>
+            <div class="col-12 col-md-3 col-sm-12 col-xs-12 m-3" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="200">
+                <img class="img-fluid img-zoom-out"
+                    src="https://i.ibb.co/TPNtKrH/Whats-App-Image-2021-09-23-at-3-05-03-PM.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="container mb-5">
+        <h2 class="wv-section-3-title" data-aos="fade-left" data-aos-duration="1000">Users Review</h2>
+        <p class="text-center mb-5">View Users Review below</p>
+        <div class="row wv-section-3">
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="50">
+                <div class="m-auto reviews">
+                    <img class="img-fluid rev-img" src="{{ asset('images/Ellipse11.svg') }}" alt="">
+                    <p class="text-center"><span>
+                            <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </span>
+                        <span class="rating">5.0</span>
+                    </p>
+                    <p class="title">Girls TV</p>
+                    <p class="desc text-white">I find it hard to get more contacts each time i paid for advert, I got low
+                        turn up, and contacts I usually get is my fellow WhatsApp TV's instead of normal viewers but the
+                        moment, I paid for wassapviews premium and follow their instructions.
+                        I hit 2k views in just a week without spending much on paid adverts🥰💃Thanks to Wassapviews</p>
+
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="100" data-aos-easing="ease-in-out">
+                <div class="m-auto reviews">
+                    <img class="img-fluid rev-img" src="{{ asset('images/Ellipse7.svg') }}" alt="">
+                    <p class="text-center"><span>
+                            <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </span>
+                        <span class="rating">5.0</span>
+                    </p>
+                    <p class="title">Cynthia</p>
+                    <p class="desc text-white">I do sell clothes online,Though I pay for WhatsApp advert, but contacts I
+                        get through those adverts are way far from where I live And way billing costs much.But since I opt
+                        in to the premium planI could get contacts of people near me.All
+                        thanks to Wassapviews</p>
+
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-sm-12 col-xs-12" data-aos="zoom-in-up" data-aos-duration="1000"
+                data-aos-delay="200">
+                <div class="m-auto reviews">
+                    <img class="img-fluid rev-img" src="{{ asset('images/Ellipse10.svg') }}" alt="">
+                    <p class="text-center"><span>
+                            <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </span>
+                        <span class="rating">5.0</span>
+                    </p>
+                    <p class="title">Gt Pounds</p>
+                    <p class="desc text-white">I trade gift cards and crypto when I pay for WhatsApp ads, the only contacts
+                        I receive are TVs and hook up girls, and it's very frustrating😭I overcomed it with the help of
+                        wassapviews, I go for the premium plan and select only boys contacts,
+                        now I have a lots of customers now🥰🥰🥰All thanks to Wassapviews</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="row newsletter-div">
+        <div class="col-12 col-md-3"></div>
+        <div class="container col col-md-6" data-aos="flip-down" data-aos-duration="1000">
+            <div class="jumbotron newsletter">
+                <h2 class="wv-section-3-title">Subscribe to our Newsletter</h2>
+                <p class="text-center mb-5">Subscribe to our newsletter to receive more updates from us</p>
+                <form action="#" method="get">
+                    <div class="form-floating">
+                        <input type="text" class="form-control input form-floating" id="name" placeholder="Enter Name:">
+                        <label for="name">Enter Name</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="email" class="form-control input form-floating" id="email" placeholder="Enter Email:">
+                        <label for="email">Enter Email</label>
+                    </div>
+                    <input type="submit" class="btn btn-success btn-lg" value="Subscribe">
+                </form>
+            </div>
+        </div>
+        <div class="col-12 col-md-3"></div>
+    </div>
+    <div class="background-primary-2 img-fluid">
+        <img src="{{ asset('images/logo.svg') }}" class="img-fluid" alt="">
+        <div class="row mt-5">
+            <div class="col-12 col-md-4">
+                <h3>Navgation Links</h3>
+                <br>
+                <ul>
+                    <li><i class="fa fa-home"></i> <a href="#">Home</a></li>
+                    <li><i class="fa fa-info"></i> <a href="#">About</a></li>
+                    <li><i class="fa fa-sign-in"></i> <a href="#">Login</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4">
+                <h3><br></h3>
+                <br>
+                <ul>
+                    <li><i class="fa fa-user-plus"></i> <a href="#">Register</a></li>
+                    <li><i class="fa fa-dashboard"></i> <a href="#">Dashboard</a></li>
+                    <li><i class="fa fa-user"></i> <a href="#">Profile</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4">
+                <h3>Social Links</h3>
+                <br>
+                <ul>
+                    <li><i class="fa fa-envelope"></i> <a href="#"> support@wassapviews.ng</a></li>
+                    <li><i class="fab fa-telegram"></i> <a href="#"> @wassapviews</a></li>
+                    <li><i class="fab fa-instagram"></i> <a href="#"> @wassapviews.ng</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+@endsection
