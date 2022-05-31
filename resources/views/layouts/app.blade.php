d213c84cb5ae67e6b965a6786f7969f8facff9b8
devcooljoe <onipedejoseph@outlook.com>
2022-05-31 13:35:13 +0100

    initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <link href="{{ route('index') }}/css/app.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ route('index') }}/css/style.css">
+    <link href="https://wassapviews-test.herokuapp.com/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://wassapviews-test.herokuapp.com/css/style.css">
 </head>
 
 <body>
